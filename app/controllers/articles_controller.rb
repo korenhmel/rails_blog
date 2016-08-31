1a890d30f0c0916043c182e381feabb714b8c162
Koren <koren.hmel@gmail.com>
2016-08-31 15:26:05 +0300

    Save articles to data and show errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,14 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    @ruby = params[:article].inspect
+    @article = Article.new(contact_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+  def contact_params
+    params.require(:article).permit(:title, :text)
   end
 end
