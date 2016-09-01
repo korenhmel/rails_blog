ccd44dc39419d6f682e527bb659960ea8080c095
Koren <koren.hmel@gmail.com>
2016-09-01 15:24:31 +0300

    Implement articles#show and redirect to this resource

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
   def new
   end
+  def show
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(contact_params)
     if @article.valid?
       @article.save
-     
+      redirect_to @article
     else
       render action: 'new'
     end
