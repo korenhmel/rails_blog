d8d09225ff3c2b1dffee0a48ed4cbb7f35762323
Koren <koren.hmel@gmail.com>
2016-10-20 20:23:36 +0300

    Add length validations and test for comment.body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   validates :autor, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 4000 }
   belongs_to :article
   belongs_to :user
 end
--- spec/models/comment/comment_spec.rb
+++ spec/models/comment/comment_spec.rb
@@ -5,4 +5,5 @@ describe Comment do
   it {should belong_to :article}
   it {should validate_presence_of :body}
   it {should validate_presence_of :autor}
+  it { should validate_length_of(:body).is_at_most(4000) }
 end
