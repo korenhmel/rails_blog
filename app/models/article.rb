01edc5d9702db5eb56b6f034359acbd50bacb604
Koren <koren.hmel@gmail.com>
2016-10-20 19:51:30 +0300

    Add validation to article length and testing it

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
   validates :text, presence: true
 
 
--- spec/models/article/article_spec.rb
+++ spec/models/article/article_spec.rb
@@ -6,7 +6,7 @@ describe Article do
 
   it {should validate_presence_of :title}
   it {should validate_presence_of :text}
-
+  it { should validate_length_of(:title).is_at_most(140) }
   end
 
   describe "associations" do
