2d47b03e8ac7d1042978fc4978a396aa62b9a531
Koren <koren.hmel@gmail.com>
2016-10-27 20:17:44 +0300

    Add to model comment length validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  validates :autor, presence: true
+  validates :autor, presence: true, length: { maximum: 145 }
   validates :body, presence: true, length: { maximum: 4000 }
   belongs_to :article
   belongs_to :user
