65d3324d4239a98e385ad59827e8692d48496138
Koren <koren.hmel@gmail.com>
2016-10-27 20:26:09 +0300

    Add tests to comments length validation

--- spec/models/comment/comment_spec.rb
+++ spec/models/comment/comment_spec.rb
@@ -6,4 +6,5 @@ describe Comment do
   it {should validate_presence_of :body}
   it {should validate_presence_of :autor}
   it { should validate_length_of(:body).is_at_most(4000) }
+  it { should validate_length_of(:autor).is_at_most(145) }
 end
