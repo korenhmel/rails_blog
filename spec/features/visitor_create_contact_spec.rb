314023b24380de13a30fd9b5274f945a5bc465fa
Koren <koren.hmel@gmail.com>
2016-10-27 21:47:20 +0300

    Update create contact test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper.rb"
 feature "Contact Creation" do
   scenario "alows access to contact page" do
     visit '/contacts'
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t('contacts.contact_us')
 
   end
 end
