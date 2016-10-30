90eaca3bd63c3c9c6a10ba850202a7771d4f8e6f
Koren <koren.hmel@gmail.com>
2016-10-30 20:36:10 +0200

    Delete method sign_up from visitor_creates_acount_spec.rb

--- spec/features/visitor_creates_acount_spec.rb
+++ spec/features/visitor_creates_acount_spec.rb
@@ -9,11 +9,3 @@ feature "Acount Creation" do
 
 end
 
-def sign_up
-  visit new_user_registration_path
-  fill_in :user_email, with: 'koren.hmel@gmail.com'
-  fill_in :user_username, with: 'koren'
-  fill_in :user_password, with: 'secure123'
-  fill_in :user_password_confirmation, with: 'secure123'
-  click_button 'Sign up'
-end
