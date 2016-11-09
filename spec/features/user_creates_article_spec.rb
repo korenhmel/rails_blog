18d5d45ea5d92b5a14b468f34381f2abbf84d11d
Koren <koren.hmel@gmail.com>
2016-11-09 18:34:02 +0200

    Code to test article comments were moved to create_comments_spec.rb

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -4,11 +4,23 @@ require "spec_helper.rb"
 feature "Article Creation" do
   before(:all) do
     sign_up
+
   end
 
-  scenario "allows user to visit new article page" do
-    visit new_article_path
-    expect(page).to have_content 'New article'
-  end
+  # scenario "allows user to visit new article page" do
+  #   visit new_article_path
+  #
+  #    expect(page).to have_content 'New article'
+  #
+  # end
+
+    scenario "user create article" do
+      visit new_article_path
+      fill_in :article_title, :with=> 'title of article'
+      fill_in :article_text, :with=> 'something gone wrong'
+      click_button 'Save Article'
+       expect(page).to have_content 'something gone wrong'
+
+    end
 
 end
